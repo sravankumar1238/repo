b049011ef67e6bf0487e20f04b198c87bc80ab5b
sravan <kara.sravan16@gmail.com>
2025-02-06 10:40:58 +0530

    new files added

---
+++ PrintNumbers.java
@@ -0,0 +1,9 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        // Loop to print numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("number: " + i);
+        }
+    }
+}
+
