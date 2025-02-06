370c1e7a1eed285964cf24ced093bb3c5fbfa624
sravan <kara.sravan16@gmail.com>
2025-02-06 16:09:42 +0530

    modified file

--- login2.java
+++ login2.java
@@ -1,5 +1,11 @@
+public class login {
+ 22 public static void main() {
+ 23     int i;
+ 24 
+ 25 
+ 26     for (i=15;i<40;i++);
+ 27         system.out.println("Number : " + i);
+ 28     }
+ 29 }
+ 30 }
 
-fhbfh
-
-wbgevbevbev
-hetcvevqejhb
