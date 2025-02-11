dc5c489717c1adc94d29d8bf3c400963cc09af2c
sravan <kara.sravan16@gmail.com>
2025-02-11 12:36:09 +0530

    modified changes

--- login2.java
+++ login2.java
@@ -3,9 +3,20 @@ public class login {
  23     int i;
  24 
  25 
- 26     for (i=40;i<40;i++);
+ 26     for (i=50;i<40;i++);
  27         system.out.println("Number : " + i);
  28     }
  29 }
  30 }
+
+public class login {
+  2  22 public static void main() {
+  3  23     int i;
+  4  24
+  5  25
+  6  26     for (i=40;i<40;i++);
+  7  27         system.ou.println("Number : " + i);
+  8  28     }
+  9  29 }
+ 10  30 }
 
